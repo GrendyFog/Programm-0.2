3fbdf75ae71c529b01b77d6b2546ca5d317896aa
Grendy_Fog <ttimmaa@gmail.com>
2019-04-19 17:12:31 +0300

    so long lessons 3

---
+++ Work/home_work(les3)_4.py
@@ -0,0 +1,5 @@
+a=input("Введите тест: ")
+b=int(len(a))
+c=int((b-4)/2)
+d=c+4
+print(a[c:d])
---
+++ Work/home_work(les3)_list.py
@@ -0,0 +1,6 @@
+mylist=[54, 11, 98, 73, 44, 15, 81]
+print(mylist)
+print(min(mylist))
+print(max(mylist))
+mylist.app (0, 0)
+print(mylist)
